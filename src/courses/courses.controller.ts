585bb63086d75e6a50c37ebe2334434bba1533db
Antonio Santos <antonio.carlos@instaletecnologia.com.br>
2025-05-27 11:57:36 -0300

    Added missing routes and methods to CoursesController

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,20 +1,39 @@
-import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
+  @Get()
+  findAll(@Res() response) {
+    return response.status(200).json({ message: 'Listagem de Cursos' });
+  }
 
-    @Get()
-    findAll(@Res() response){
-        return response.status(200).json({message:"Listagem de Cursos"});
-    }
-    
-    @Get(':id/:name')
-    findOne(@Param('id') id:string, @Param('name') name:string){
-        return `Curso ${id} - ${name}`;
-    }
-    
-    @Post()
-    create(@Body() body){
-        return body;
-    }
+  @Get(':id/:name')
+  findOne(@Param('id') id: string, @Param('name') name: string) {
+    return `Curso ${id} - ${name}`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    console.log(body);
+    return `Atualizando curso ${id}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Removendo curso ${id}`;
+  }
 }
